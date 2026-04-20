8c8d6e85394ae2d029d1557f82e1b3579d1b5ca0
karaulkin <www.tema15@gmail.com>
2026-04-20 17:58:06 +0300

    completed the descriptor task

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,32 @@ class Model:
 
 
 class Field:
-    pass
+    def __init__(self, path: str):
+        self.path = path
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        keys = self.path.split('.')
+        data = instance.payload
+
+        for key in keys:
+            if isinstance(data, dict) and key in data:
+                data = data[key]
+            else:
+                return None
+        return data
+
+    def __set__(self, instance, value):
+        keys = self.path.split('.')
+        data = instance.payload
+
+        for key in keys[:-1]:
+            data = data[key]
+
+        last_key = keys[-1]
+        data[last_key] = value
