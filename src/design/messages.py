209375ba2bdb4688058fb22046d4a38403a3cbb1
karaulkin <www.tema15@gmail.com>
2026-04-20 19:10:18 +0300

    completed the messages task

--- src/design/messages.py
+++ src/design/messages.py
@@ -1,5 +1,6 @@
 import enum
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 
 class MessageType(enum.Enum):
@@ -17,3 +18,43 @@ class JsonMessage:
 @dataclass
 class ParsedMessage:
     """There is no need to describe anything here."""
+
+
+class BaseParser(ABC):
+    @abstractmethod
+    def parse(self, message: JsonMessage) -> ParsedMessage:
+        pass
+
+
+class TelegramParser(BaseParser):
+    def parse(self, message: JsonMessage) -> ParsedMessage:
+        ## TODO: логика парсинга из Telegram
+        return ParsedMessage()
+
+
+class SlackParser(BaseParser):
+    def parse(self, message: JsonMessage) -> ParsedMessage:
+        ## TODO: логика парсинга из Slack
+        return ParsedMessage()
+
+
+class MattermostParser(BaseParser):
+    def parse(self, message: JsonMessage) -> ParsedMessage:
+        ## TODO: логика парсинга из Mattermost
+        return ParsedMessage()
+
+
+class ParserFactory:
+    parsers = {
+        MessageType.TELEGRAM: TelegramParser,
+        MessageType.SLACK: SlackParser,
+        MessageType.MATTERMOST: MattermostParser
+    }
+
+    @classmethod
+    def get_parser(cls, message_type: MessageType) -> BaseParser:
+        parser_class = cls.parsers.get(message_type)
+        if parser_class is None:
+            raise ValueError(f'Неизвестный тип {message_type}')
+
+        return parser_class()
